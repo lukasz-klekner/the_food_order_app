ab5e4acfc6cfaa3d323bb198de55aa6303d17845
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-03 16:51:56 +0100

    fixes add and remove Cart items fn in context

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -9,9 +9,55 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD': {
-      const updatedItems = [...state.items, action.item]
       const updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount
+
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      )
+
+      const existingCartItem = state.items[existingCartItemIndex]
+      let updatedItems
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        }
+
+        updatedItems = [...state.item]
+        updatedItems[existingCartItemIndex] = updatedItem
+      } else {
+        updatedItems = [...state.items, action.item]
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      }
+    }
+
+    case 'REMOVE': {
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.id
+      )
+
+      const existingCartItem = state.items[existingCartItemIndex]
+      const updatedTotalAmount = state.totalAmount - existingCartItem.price
+
+      let updatedItems
+
+      if (existingCartItem.amount === 1) {
+        updatedItems = state.items.filter((item) => item.id !== action.id)
+      } else {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount - 1,
+        }
+
+        updatedItems = [...state.items]
+        updatedItems[existingCartItemIndex] = updatedItem
+      }
 
       return {
         items: updatedItems,
@@ -33,10 +79,15 @@ const CartProvider = ({ children }) => {
   const addItemToCartHandler = (item) =>
     dispatchCartAction({ type: 'ADD', item })
 
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id })
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   }
 
   return (
